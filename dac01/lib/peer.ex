09ebc31f34b0c0c8136cafc5dfeb5756f6304efe
Alexandru Toma <bartimaeus1073@gmail.com>
2018-02-01 01:25:11 +0000

    Updated peer

--- dac01/lib/peer.ex
+++ dac01/lib/peer.ex
@@ -3,41 +3,49 @@
 defmodule Peer do
 
   def start(peer_id) do
-    receive do 
+    receive do
       {:bind, neighbours} -> next(peer_id, neighbours)
     end
   end
 
-  def next(peer_id, neighbours) do
+  defp next(peer_id, neighbours) do
     receive do
-      {:broadcast, max_broadcasts, timeout} -> 
+      {:broadcast, max_broadcasts, timeout} ->
         data = for _ <- neighbours, do: {0, 0}
         start_broadcast(peer_id, neighbours, max_broadcasts, timeout, data)
     end
   end
 
-  def start_broadcast(peer_id, neighbours, max_broadcasts, timeout, data) do
-    IO.puts "#{inspect(data)}"
-
-    data = receive do
-      {:send} ->
-        {first, second} = Enum.at(data, peer_id)
-        if second < max_broadcasts do
-        end
-    end
-        
+  defp start_broadcast(peer_id, neighbours, max_broadcasts, timeout, data) do
     {first, second} = Enum.at(data, peer_id)
+
     if first < max_broadcasts do
       for n <- neighbours do
-        send n, {:send}
+        send n, {:send, peer_id}
+        replace_data_with(data, n + 1, {first + 1, second})
       end
     end
-    
-    start_broadcast(peer_id, neighbours, max_broadcasts, timeout, data)
+
+    receive do
+      {:send, from} ->
+        {first, second} = Enum.at(data, peer_id)
+        if second < max_broadcasts do
+          # replace_data_with(data, from + 1, {first, second + 1})
+        end
+    end
+
+    # start_broadcast(peer_id, neighbours, max_broadcasts, timeout, data)
+    display_data(peer_id, data)
   end
 
+  # function that replaces an item in a list at index (1-indexed)
   def replace_data_with(data, index, tuple) do
-    :lists.sublist(data, index) ++ tuple ++ :lists.sublist(data, index + 2, length(data))
+    :lists.sublist(data, index-1) ++ [tuple] ++
+          :lists.sublist(data, index+1, length(data))
+  end
+
+  def display_data(peer_id, data) do
+    IO.puts "#{peer_id}: #{inspect(data)}"
   end
 
 end
