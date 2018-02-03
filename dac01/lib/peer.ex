80bdb99e35b11229b6a6b3ee404cf7476c53f182
Alexandru Toma <bartimaeus1073@gmail.com>
2018-02-03 18:53:49 +0000

    Finished task 1

--- dac01/lib/peer.ex
+++ dac01/lib/peer.ex
@@ -40,8 +40,6 @@ defmodule Peer do
           List.replace_at(receive_state, from, Enum.at(receive_state, from) + 1)
       end
 
-      IO.puts inspect receive_state
-
       receive_peer(peer_id, neighbours, max_broadcasts, timeout, new_state, parent_process)
     end
   end
@@ -65,10 +63,10 @@ defmodule Peer do
     sendP = spawn(Peer, :send_peer, [peer_id, neighbours, max_broadcasts, timeout, send_state, self()])
     receiveP = spawn(Peer, :receive_peer, [peer_id, neighbours, max_broadcasts, timeout, receive_state, self()])
 
-    run_broadcast(peer_id, false, false, send_state, receive_state)
+    run_broadcast(peer_id, false, false, send_state, receive_state, receiveP)
   end
 
-  defp run_broadcast(peer_id, send_stopped, receive_stopped, send_state, receive_state) do
+  defp run_broadcast(peer_id, send_stopped, receive_stopped, send_state, receive_state, receiveP) do
     if send_stopped and receive_stopped do
       end_broadcast(peer_id, Enum.zip(send_state, receive_state))
     end
@@ -78,49 +76,14 @@ defmodule Peer do
         {true, receive_stopped, state, receive_state}
       {:stop_receive, state} ->
         {send_stopped, true, send_state, state}
+      {:send, content} ->
+        # redirect to receive process
+        send receiveP, {:send, content}
+        {send_stopped, receive_stopped, send_state, receive_state}
     end
 
-    run_broadcast(peer_id, new_send_stopped, new_receive_stopped, new_send_state, new_receive_state)
+    run_broadcast(peer_id, new_send_stopped, new_receive_stopped, new_send_state, new_receive_state, receiveP)
   end
-
-  # defp start_broadcast(peer_id, neighbours, max_broadcasts, timeout, state) do
-  #   # check if there are any receive or send left
-  #   res = for i <- 0..length(neighbours)-1 do
-  #     {first, second} = Enum.at(state, i)
-  #     if first < max_broadcasts || second < max_broadcasts do
-  #       True
-  #     else
-  #       False
-  #     end
-  #   end
-  #
-  #   # if threre are no more sends or receive to be made then print the state
-  #   if !Enum.reduce(res, fn(b1, b2) -> b1 || b2 end) do
-  #     end_broadcast(peer_id, state)
-  #   else
-  #     for i <- 0..length(neighbours)-1 do
-  #       {first, second} = Enum.at(state, i)
-  #
-  #       if first < max_broadcasts do
-  #         state = List.replace_at(state, peer_id, {first + 1, second})
-  #         send Enum.at(neighbours, i), {:send, peer_id, state}
-  #         end_broadcast(peer_id, state)
-  #       end
-  #     end
-  #
-  #     receive do
-  #      {:send, from, prevState} ->
-  #        {first, second} = Enum.at(prevState, from)
-  #
-  #        if second < max_broadcasts do
-  #          state = List.replace_at(state, from, {first, second + 1})
-  #          end_broadcast(peer_id, state)
-  #        end
-  #     end
-  #
-  #     start_broadcast(peer_id, neighbours, max_broadcasts, timeout, state)
-  #   end
-  # end
 
   # prints the state of the peer
   defp end_broadcast(peer_id, state) do
