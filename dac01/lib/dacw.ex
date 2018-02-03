0f13261f6498c2b9552d8ca3a8cffa03d20fe93c
Alexandru Toma <bartimaeus1073@gmail.com>
2018-02-03 19:33:11 +0000

    Added timeout

--- dac01/lib/dacw.ex
+++ dac01/lib/dacw.ex
@@ -3,14 +3,57 @@
 defmodule Dacw do
 
   def main() do
-    start_system1()
+    start_system1(true)
   end
 
-  defp start_system1() do
+  # Local tests
+  # Test 1
+  # nr_of_peers = 5
+  # max_broadcasts = 1000
+  # timeout = 3000
+  # Output:
+  # 0: [{1000, 1000}, {1000, 1000}, {1000, 1000}, {1000, 1000}, {1000, 1000}]
+  # 2: [{1000, 1000}, {1000, 1000}, {1000, 1000}, {1000, 1000}, {1000, 1000}]
+  # 3: [{1000, 1000}, {1000, 1000}, {1000, 1000}, {1000, 1000}, {1000, 1000}]
+  # 4: [{1000, 1000}, {1000, 1000}, {1000, 1000}, {1000, 1000}, {1000, 1000}]
+  # 1: [{1000, 1000}, {1000, 1000}, {1000, 1000}, {1000, 1000}, {1000, 1000}]
+  #
+  # Test 2
+  # nr_of_peers = 5
+  # max_broadcasts = 10_000_000
+  # timeout = 3000
+  # Output:
+  # 1: [{93154, 87113}, {93154, 93153}, {93154, 146710}, {93154, 107294}, {93154, 53965}]
+  # 4: [{53966, 87111}, {53966, 93151}, {53966, 146704}, {53966, 107292}, {53966, 53964}]
+  # 2: [{146714, 87109}, {146714, 93149}, {146714, 146697}, {146714, 107284}, {146714, 53961}]
+  # 3: [{107296, 87031}, {107296, 93072}, {107296, 146480}, {107296, 107147}, {107296, 53883}]
+  # 0: [{87114, 87040}, {87114, 93081}, {87114, 146504}, {87114, 107166}, {87114, 53893}]
+  #
+  # Test 3
+  # nr_of_peers = 10
+  # max_broadcasts = 10_000_000
+  # timeout = 3000
+  # Output:
+  g 18705}, {18636, 18635}, {18636, 13924}, {18636, 20476}, {18636, 18589}, {18636, 18250}, {18636, 12688}, {18636, 16539}, {18636, 11876}, {18636, 18228}]
+  defp start_system1(is_local) do
     nr_of_peers = 5
     max_broadcasts = 1000
     timeout = 3000
-    spawn(System1, :start, [true, nr_of_peers, max_broadcasts, timeout])
+    spawn(System1, :start, [is_local, nr_of_peers, max_broadcasts, timeout])
+
+    Process.sleep(timeout)
+
+    nr_of_peers = 5
+    max_broadcasts = 10_000_000
+    timeout = 3000
+    spawn(System1, :start, [is_local, nr_of_peers, max_broadcasts, timeout])
+
+    Process.sleep(timeout)
+
+    nr_of_peers = 10
+    max_broadcasts = 10_000_000
+    timeout = 3000
+    spawn(System1, :start, [is_local, nr_of_peers, max_broadcasts, timeout])
   end
 
 end
--- dac01/lib/peer_system1.ex
+++ dac01/lib/peer_system1.ex
@@ -2,11 +2,18 @@
 
 defmodule PeerSystem1 do
 
+  def has_exceeded_timeout(timeout, start_time) do
+    now = :os.system_time(:milli_seconds)
+    (now - start_time) > timeout
+  end
+
   # send state [0, 0, 0, 0, 0]
-  def send_peer(peer_id, neighbours, max_broadcasts, timeout, send_state, parent_process) do
+  def send_peer(peer_id, neighbours, max_broadcasts, timeout, send_state, parent_process, start_time) do
     should_stop = not Enum.reduce(for i <- 0..length(neighbours)-1 do
       Enum.at(send_state, i) < max_broadcasts
     end, fn(x, y) -> x or y end)
+
+    should_stop = should_stop or has_exceeded_timeout(timeout, start_time)
 
     # stop broadcst
     if should_stop do
@@ -22,15 +29,17 @@ defmodule PeerSystem1 do
         end
       end
 
-      send_peer(peer_id, neighbours, max_broadcasts, timeout, new_state, parent_process)
+      send_peer(peer_id, neighbours, max_broadcasts, timeout, new_state, parent_process, start_time)
     end
   end
 
   # receive state [0, 0, 0, 0, 0]
-  def receive_peer(peer_id, neighbours, max_broadcasts, timeout, receive_state, parent_process) do
+  def receive_peer(peer_id, neighbours, max_broadcasts, timeout, receive_state, parent_process, start_time) do
     should_stop = not Enum.reduce(for i <- 0..length(neighbours)-1 do
       Enum.at(receive_state, i) < max_broadcasts
     end, fn(x, y) -> x or y end)
+
+    should_stop = should_stop or has_exceeded_timeout(timeout, start_time)
 
     if should_stop do
       send parent_process, {:stop_receive, receive_state}
@@ -40,28 +49,29 @@ defmodule PeerSystem1 do
           List.replace_at(receive_state, from, Enum.at(receive_state, from) + 1)
       end
 
-      receive_peer(peer_id, neighbours, max_broadcasts, timeout, new_state, parent_process)
+      receive_peer(peer_id, neighbours, max_broadcasts, timeout, new_state, parent_process, start_time)
     end
   end
 
   def start(peer_id) do
+    start_time = :os.system_time(:milli_seconds)
     receive do
-      {:bind, neighbours} -> next(peer_id, neighbours)
+      {:bind, neighbours} -> next(peer_id, neighbours, start_time)
     end
   end
 
-  defp next(peer_id, neighbours) do
+  defp next(peer_id, neighbours, start_time) do
     receive do
       {:broadcast, max_broadcasts, timeout} ->
-        start_broadcast(peer_id, neighbours, max_broadcasts, timeout)
+        start_broadcast(peer_id, neighbours, max_broadcasts, timeout, start_time)
     end
   end
 
-  defp start_broadcast(peer_id, neighbours, max_broadcasts, timeout) do
+  defp start_broadcast(peer_id, neighbours, max_broadcasts, timeout, start_time) do
     send_state = for _ <- 0..length(neighbours)-1, do: 0
     receive_state = for _ <- 0..length(neighbours)-1, do: 0
-    spawn(PeerSystem1, :send_peer, [peer_id, neighbours, max_broadcasts, timeout, send_state, self()])
-    receiveP = spawn(PeerSystem1, :receive_peer, [peer_id, neighbours, max_broadcasts, timeout, receive_state, self()])
+    spawn(PeerSystem1, :send_peer, [peer_id, neighbours, max_broadcasts, timeout, send_state, self(), start_time])
+    receiveP = spawn(PeerSystem1, :receive_peer, [peer_id, neighbours, max_broadcasts, timeout, receive_state, self(), start_time])
 
     run_broadcast(peer_id, false, false, send_state, receive_state, receiveP)
   end
@@ -69,20 +79,20 @@ defmodule PeerSystem1 do
   defp run_broadcast(peer_id, send_stopped, receive_stopped, send_state, receive_state, receiveP) do
     if send_stopped and receive_stopped do
       end_broadcast(peer_id, Enum.zip(send_state, receive_state))
-    end
+    else
+      {new_send_stopped, new_receive_stopped, new_send_state, new_receive_state} = receive do
+        {:stop_send, state} ->
+          {true, receive_stopped, state, receive_state}
+        {:stop_receive, state} ->
+          {send_stopped, true, send_state, state}
+        {:send, content} ->
+          # redirect to receive process
+          send receiveP, {:send, content}
+          {send_stopped, receive_stopped, send_state, receive_state}
+      end
 
-    {new_send_stopped, new_receive_stopped, new_send_state, new_receive_state} = receive do
-      {:stop_send, state} ->
-        {true, receive_stopped, state, receive_state}
-      {:stop_receive, state} ->
-        {send_stopped, true, send_state, state}
-      {:send, content} ->
-        # redirect to receive process
-        send receiveP, {:send, content}
-        {send_stopped, receive_stopped, send_state, receive_state}
+      run_broadcast(peer_id, new_send_stopped, new_receive_stopped, new_send_state, new_receive_state, receiveP)
     end
-
-    run_broadcast(peer_id, new_send_stopped, new_receive_stopped, new_send_state, new_receive_state, receiveP)
   end
 
   # prints the state of the peer
