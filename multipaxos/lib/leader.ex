a3169d8a876ea8c11f680f381c67b372b6c212b3
Alexandru Toma <bartimaeus1073@gmail.com>
2018-02-18 18:15:07 +0000

    Acceptros

--- multipaxos/lib/leader.ex
+++ multipaxos/lib/leader.ex
@@ -1,4 +1,30 @@
 defmodule Leader do
   def start(config) do
+    receive do
+      {:bind, acceptors, replicas} ->
+        acceptors = MapSet.new(acceptors)
+        replicas = MapSet.new(replicas)
+        ballot_num = {0, self()}
+        spawn(Scout, :start, [self(), acceptors, ballot_num])
+        next(config, acceptors, replicas, ballot_num, false, Map.new)
+    end
+  end
+
+  def next(config, acceptors, replicas, ballot_num, active, proposals) do
+    receive do
+      {:propose, slot, command} ->
+        next(config, acceptors, replicas, ballot_num, active, proposals)
+      {:adopted, ballot, pvalues} ->
+        next(config, acceptors, replicas, ballot_num, active, proposals)
+      {:preempted, {seq, leader}=ballot} ->
+        if ballot > ballot_num do
+          active = false
+          ballot_num = {seq + 1, self()}
+          spawn(Scout, :start, [self(), acceptors, ballot_num])
+          next(config, acceptors, replicas, ballot_num, active, proposals)
+        else
+          next(config, acceptors, replicas, ballot_num, active, proposals)
+        end
+    end
   end
 end
--- multipaxos/lib/scout.ex
+++ multipaxos/lib/scout.ex
@@ -6,16 +6,16 @@ defmodule Scout do
     next(leader, acceptors, ballot_num, acceptors, MapSet.new)
   end
 
-  def next(leader, acceptros, ballot_num, waitfor, pvalues) do
+  def next(leader, acceptors, ballot_num, waitfor, pvalues) do
     receive do
       {:p1b, acceptor, ballot, acc_pvalues} ->
         if ballot == ballot_num do
           pvalues = MapSet.union(pvalues, acc_pvalues)
           waitfor = MapSet.delete(waitfor, acceptor)
-          if MapSet.size(waitfor) < MapSet.size(acceptor) / 2 do
+          if MapSet.size(waitfor) < MapSet.size(acceptors) / 2 do
             send leader, {:adopted, ballot, pvalues}
           else
-            next(leader, acceptros, ballot_num, waitfor, pvalues)
+            next(leader, acceptors, ballot_num, waitfor, pvalues)
           end
         else
           send leader, {:preempted, ballot}
