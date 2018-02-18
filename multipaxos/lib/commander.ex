616a8ee52661a591eeddbfcae6616be284b5cf3b
Alexandru Toma <bartimaeus1073@gmail.com>
2018-02-18 18:00:25 +0000

    Commander

--- multipaxos/lib/commander.ex
+++ multipaxos/lib/commander.ex
@@ -1,4 +1,27 @@
 defmodule Commander do
-  def start() do
+  def start(leader, acceptors, replicas, pvalue) do
+    Enum.map(acceptors, fn(acc) ->
+        send acc, {:p2a, self(), pvalue}
+      end)
+    next(leader, acceptors, replicas, pvalue, acceptors)
+  end
+
+  def next(leader, acceptors, replicas, pvalue, waitfor) do
+    {ballot_num, slot, command} = pvalue
+    receive do
+      {:p2b, acceptor, ballot} ->
+        if ballot == ballot_num do
+          waitfor = MapSet.delete(waitfor, acceptor)
+          if MapSet.size(waitfor) < MapSet.size(acceptors) / 2 do
+            Enum.map(replicas, fn(rep) ->
+                send rep, {:decision, slot, command}
+              end)
+          else
+            next(leader, acceptors, replicas, pvalue, waitfor)
+          end
+        else
+          send leader, {:preempted, ballot}
+        end
+    end
   end
 end
